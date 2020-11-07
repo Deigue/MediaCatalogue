30d31fa9d888737ff6bfee6a220ec847a9fe5c6b
Deigue <deigue@hotmail.com>
2020-11-06 21:58:42 -0500

    File picker VM to substitute file open/save invokes anywhere.

---
+++ MediaCatalogue/ViewModels/FilePickerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MediaCatalogue.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaCatalogue.ViewModels
+{
+    // To be replacing as util VM for all places where file picker needs to be utilized.
+    class FilePickerViewModel : ViewModel
+    {
+        public FilePickerViewModel()
+        {
+
+        }
+    }
+}
