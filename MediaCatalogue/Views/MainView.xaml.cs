db054d08a763149df988da6b98f0b9180cd8d4a6
Deigue <deigue@hotmail.com>
2020-11-13 22:55:18 -0500

    minor refactorings

--- MediaCatalogue/Views/MainView.xaml.cs
+++ MediaCatalogue/Views/MainView.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using MediaCatalogue.ViewModels;
 using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MediaCatalogue.Views
 {
@@ -28,7 +14,7 @@ namespace MediaCatalogue.Views
         public MainView()
         {
             InitializeComponent();
-            this.WhenActivated(diposable =>
+            this.WhenActivated(disposable =>
             {
                 this.WhenAnyValue(view => view.ViewModel!.ActivePane)
                     .ObserveOn(RxApp.MainThreadScheduler)
