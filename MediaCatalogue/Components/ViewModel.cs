2226ae2ec66b9fac655ff36d3d24c2662cca60ff
Deigue <deigue@hotmail.com>
2020-11-27 01:50:14 -0500

    Add capability to build composite disposable for every generic vm

--- MediaCatalogue/Components/ViewModel.cs
+++ MediaCatalogue/Components/ViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using ReactiveUI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MediaCatalogue.Components
 {
-    public class ViewModel : ReactiveObject
+    public class ViewModel : ReactiveObject, IDisposable
     {
+        private readonly Lazy<CompositeDisposable> _compositeDisposable = new Lazy<CompositeDisposable>();
+
+        public CompositeDisposable CompositeDisposable => _compositeDisposable.Value;
+        public void Dispose()
+        {
+            if (_compositeDisposable.IsValueCreated) _compositeDisposable.Value.Dispose();
+        }
     }
 }
